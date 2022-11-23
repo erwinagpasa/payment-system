0736f2813532c9f264882e2dd31a029b27b509e1
erwinagpasa <istran.net@gmail.com>
2022-11-23 14:59:13 +0300

    adding router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import Home from "./components/ui/Home";
+import Dashboard from "./components/ui/Dashboard";
+import Errorpage from "./components/ui/Errorpage";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="*" element={<Errorpage />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/ui/Errorpage.js
@@ -0,0 +1,9 @@
+
+const Errorpage = () =>{
+  return(
+  <div>
+    <h1>404 page</h1>
+  </div>
+    );
+}
+export default Errorpage;
